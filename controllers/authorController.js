f857881cdee1b5e3e2178e37c3ce6b83c6979ae6
Flow <raja.mywebsite123@gmail.com>
2023-07-07 01:30:26 +0200

    author_detail

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,18 +1,37 @@
 const Author = require("../models/author");
+const Book = require("../models/book");
 const asyncHandler = require("express-async-handler");
 
 // Anzeige der Liste aller Autoren.
 exports.author_list = asyncHandler(async (req, res, next) => {
-    const allAuthors = await Author.find().sort({ family_name: 1 }).exec();
-    res.render("author_list", {
-      title: "Autorenliste",
-      author_list: allAuthors,
-    });
+  const allAuthors = await Author.find().sort({ family_name: 1 }).exec();
+  res.render("author_list", {
+    title: "Autorenliste",
+    author_list: allAuthors,
   });
+});
 
 // Detailseite für einen bestimmten Autor anzeigen.
 exports.author_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NICHT IMPLEMENTIERT: Autorendetails: ${req.params.id}`);
+  try {
+    const author = await Author.findById(req.params.id);
+    const books = await Book.find({ author: req.params.id }).exec();
+
+    if (author === null) {
+      // Kein Autor gefunden.
+      const err = new Error("Autor nicht gefunden");
+      err.status = 404;
+      return next(err);
+    }
+
+    res.render("author_detail", {
+      title: "Autordetail",
+      author: author,
+      books: books,
+    });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 // Anzeigen des Autoren-Erstellungsformulars bei GET.
